b25ae7306b8d0b7e8f5826855ca4ef3020ccd652
Alexander J. Hoffmann <alex@castalia.io>
2016-07-08 00:53:53 +0000

    Vapor updates

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,9 +1,15 @@
 from flask_wtf import Form
-from wtforms import StringField, SubmitField
-from wtforms.validators import Required
+from wtforms import StringField, SubmitField, PasswordField, BooleanField,\
+    SubmitField
+from wtforms.validators import Required, Length, Email
 
 class NameForm(Form):
     name = StringField("What is your name, friend?", validators=[Required()])
     submit = SubmitField('SUBMIT')
     
-    
+class LoginForm(Form):
+    email = StringField('Email', validators=[Required(), Length(1, 64), 
+                                                                    Email()])
+    password = PasswordField('Password', validators=[Required()])
+    remember_me = BooleanField('Keep me logged in to Vapor Land')
+    submit = SubmitField('Log In')
