5a3a805a8cd476eba36e60429b9619dae70c0a63
Alexander J. Hoffmann <alex@castalia.io>
2016-07-10 17:50:13 +0000

    Vapor updates

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,17 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
-#from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 from config import config
+import os
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] =\
+    'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 
-
+db = SQLAlchemy(app)
 bootstrap = Bootstrap()
-#db = SQLAlchemy()
-
 
 def create_app(config_name):
     app = Flask(__name__)
@@ -15,7 +19,7 @@ def create_app(config_name):
     config[config_name].init_app(app)
     
     bootstrap.init_app(app)
-  #  db.init_app(app)
+    db.init_app(app)
   
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
--- app/models.py
+++ app/models.py
@@ -2,17 +2,11 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask import Flask
 import os
+from __init__ import db
 
-basedir = os.path.abspath(os.path.dirname(__file__))
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] =\
-    'sqlite:///' + os.path.join(basedir, 'data.sqlite')
-app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
-
-db = SQLAlchemy(app)
-
-class Role(db.model):
+class Role(db.Model):
     __tablename__ = 'roles'
+    extend_existing=True
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), unique=True)
     
@@ -25,8 +19,21 @@ class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), unique=True, index=True)
+    password_hash = db.Column(db.String(128))
     
     def __repr(self):
         return '<Role %r>' % self.name
     
-    role_id = db.Column(db.integer, db.ForeignKey('roles.id0'))
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+    
+    @property
+    def password(self):
+        raise AttributeError('Password is not a readable attribute')
+    
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+    
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
+        
