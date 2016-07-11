5ab54a1fe519976fdbfc50053f8724b152651bdb
Alexander J. Hoffmann <alex@castalia.io>
2016-07-11 02:03:47 +0000

    Vapor updates

--- app/auth/views.py
+++ app/auth/views.py
@@ -2,7 +2,8 @@ from flask import render_template, redirect, request, url_for, flash
 from flask_login import login_user, logout_user, login_required
 from . import auth
 from ..models import User
-from .forms import LoginForm
+from .. import db
+from .forms import LoginForm, RegistrationForm
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
@@ -21,3 +22,16 @@ def logout():
     logout_user()
     flash('You have been logged out of VAPOR LAND')
     return redirect(url_for('main.index'))
+    
+@auth.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, password=form.password.data)
+        db.session.add(user)
+        flash('You can now login')
+        return redirect(url_for('auth.login'))
+    return render_template('auth/register.html', form=form)
+    
+    
+    
