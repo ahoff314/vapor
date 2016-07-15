79a3815725b7b6cdeb9fa5103d968360dfa349db
Alexander J. Hoffmann <alex@castalia.io>
2016-07-15 03:47:14 +0000

    vapor updates

--- app/__init__.py
+++ app/__init__.py
@@ -23,23 +23,22 @@ login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
 
+
 def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config[config_name])
     config[config_name].init_app(app)
-    
+
     bootstrap.init_app(app)
     db.init_app(app)
     pagedown.init_app(app)
-  
+
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
-    
+
     from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint, url_prefix='/auth')
-    
+
     login_manager.init_app(app)
-    
+
     return app
-    
-    
--- app/models.py
+++ app/models.py
@@ -10,22 +10,23 @@ import bleach
 
 from . import db, login_manager
 
+
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
-    
-    
+
+
 class Role(db.Model):
     __tablename__ = 'roles'
-    extend_existing=True
+    extend_existing = True
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), unique=True)
     users = db.relationship('User', backref='role', lazy='dynamic')
-    
+
     def __repr__(self):
         return '<Role %r>' % self.name
 
-    
+
 class User(UserMixin, db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
@@ -35,18 +36,18 @@ class User(UserMixin, db.Model):
     password_hash = db.Column(db.String(128))
     posts = db.relationship('Post', backref='author', lazy='dynamic')
     comments = db.relationship('Comment', backref='author', lazy='dynamic')
-    
+
     def __repr__(self):
         return '<User %r>' % self.username
-    
+
     @property
     def password(self):
         raise AttributeError('Password is not a readable attribute')
-    
+
     @password.setter
     def password(self, password):
         self.password_hash = generate_password_hash(password)
-    
+
     def verify_password(self, password):
         return check_password_hash(self.password_hash, password)
 
@@ -58,7 +59,7 @@ class Post(db.Model):
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     author_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     comments = db.relationship('Comment', backref='post', lazy='dynamic')
-    
+
 
 class Comment(db.Model):
     __tablename__ = 'comments'
@@ -69,7 +70,7 @@ class Comment(db.Model):
     disabled = db.Column(db.Boolean)
     author_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     post_id = db.Column(db.Integer, db.ForeignKey('posts.id'))
-    
+
     @staticmethod
     def on_changed_body(target, value, oldvalue, initiator):
         allowed_tags = ['a', 'abbr', 'acronym', 'b', 'code', 'em', 'i',
@@ -77,8 +78,6 @@ class Comment(db.Model):
         target.body_html = bleach.linkify(bleach.clean(
             markdown(value, output_format='html'),
             tags=allowed_tags, strip=True))
-    
-    
-db.event.listen(Comment.body, 'set', Comment.on_changed_body)    
-   
-    
+
+
+db.event.listen(Comment.body, 'set', Comment.on_changed_body)
