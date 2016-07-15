a309920e46025424a6c1cadeae733117a53098ec
Alexander J. Hoffmann <alex@castalia.io>
2016-07-15 01:33:37 +0000

    vapor updates

--- manage.py
+++ manage.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python
 import os
-#from app import create_app
-#from flask.ext.sqlalchemy import SQLAlchemy
+
 from flask import Flask
+
+from app import create_app
 
 COV = None
 if os.environ.get('FLASK_COVERAGE'):
     import coverage
     COV = coverage.coverage(branch=True, include='app/*')
     COV.start()
+
 
 if os.path.exists('.env'):
     print('Importing environment from .env...')
@@ -17,15 +19,8 @@ if os.path.exists('.env'):
         if len(var) == 2:
             os.environ[var[0]] = var[1]
 
-from app import create_app
-
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
-#manager = Manager(app)
-#migrate = Migrate(app, db)
-
-
-
 
 host = os.environ.get('IP', '0.0.0.0')
 port = int(os.environ.get('PORT', 8080))
