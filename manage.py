91269120c71ea1812e876e3742c96b6561773df2
Alexander J. Hoffmann <alex@castalia.io>
2016-07-20 02:50:15 +0000

    vapor updates

--- manage.py
+++ manage.py
@@ -21,10 +21,30 @@ if os.path.exists('.env'):
 
 from app import create_app, db
 from app.models import User, Role, Post, Comment
+from flask_script import Manager
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
+manager= Manager(app)
+
+@manager.command
+def profile(length=25, profile_dir=None):
+    """Start the application under the code profiler."""
+    from werkzeug.contrib.profiler import ProfilerMiddleware
+    app.wsgi_app = ProfilerMiddleware(app.wsgi_app, restrictions=[length],
+                                      profile_dir=profile_dir)
+    app.run()
+
+
+@manager.command
+def deploy():
+    """Run deployment tasks."""
+    from flask_migrate import upgrade
+    from app.models import Role, User
+
+    # migrate database to latest revision
+    upgrade()
 
 #host = os.environ.get('IP', '0.0.0.0')
 #port = int(os.environ.get('PORT', 8080))
 if __name__ == "__main__":
-    app.run()
+    manager.run() # host=host, port=port
