2e98addda6c80acb835c17a02e9e1b64ada36836
Alexander J. Hoffmann <alex@castalia.io>
2016-07-20 00:54:36 +0000

    vapor updates

--- config.py
+++ config.py
@@ -1,6 +1,19 @@
 import os
+import psycopg2
+import urlparse
+
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+urlparse.uses_netloc.append("postgres")
+url = urlparse.urlparse(os.environ["DATABASE_URL"])
+
+conn = psycopg2.connect(
+    database=url.path[1:],
+    user=url.username,
+    password=url.password,
+    host=url.hostname,
+    port=url.port
+)
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
